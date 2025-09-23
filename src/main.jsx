5ee29f2e1a55ee9abddcdd78df36b3aff01050fb
Mattias Baldi <mattiasbaldi@gmail.com>
2025-09-23 23:51:02 +0200

    added sentry sdk config

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,13 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import * as Sentry from "@sentry/react";
+import App from "./App.jsx";
 
 Sentry.init({
   dsn: "https://759f5e5952521fd5055422c37be9eab2@o4510067874398208.ingest.de.sentry.io/4510070944956496",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
   sendDefaultPii: true,
 });
 
-console.warn("test");
-
 const container = document.getElementById("root");
-const root = createRoot(container, {
-  // Callback called when an error is thrown and not caught by an ErrorBoundary.
-  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
-    console.warn("Uncaught error", error, errorInfo.componentStack);
-  }),
-  // Callback called when React catches an error in an ErrorBoundary.
-  onCaughtError: Sentry.reactErrorHandler(),
-  // Callback called when React automatically recovers from errors.
-  onRecoverableError: Sentry.reactErrorHandler(),
-});
-
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const root = createRoot(container);
+root.render(<App />);
